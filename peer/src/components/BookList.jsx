44975bc846cc572dfe557d7da21317b9a319dc67
Eelismey-jf <80247840+Eelismey-jf@users.noreply.github.com>
2021-04-28 23:06:17 +0200

    Create BookList.jsx

---
+++ peer/src/components/BookList.jsx
@@ -0,0 +1,90 @@
+import { CardColumns, Card } from "react-bootstrap";
+import FantasyBooks from "../fantasy-books.json";
+
+const ShowBooks = (props) => {
+  return FantasyBooks.map((fantasies) => (
+    <CardColumns keys={fantasies.asin}>
+      <h2>New Releases of {props.getMonth}</h2>
+      <Card>
+        <Card.Img variant="top" src={fantasies.img} />
+        <Card.Body>
+          <Card.Title>{fantasies.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </CardColumns>
+  ));
+};
+
+export default ShowBooks;
+
+/***
+ * /****************
+ </Card>
+<Card>
+<Card.Img variant="top" src="holder.js/100px160" />
+<Card.Body>
+  <Card.Title>Card title</Card.Title>
+  <Card.Text>
+    This card has supporting text below as a natural lead-in to additional
+    content.{' '}
+  </Card.Text>
+</Card.Body>
+<Card.Footer>
+  <small className="text-muted">Last updated 3 mins ago</small>
+</Card.Footer>
+</Card>
+<Card bg="primary" text="white" className="text-center p-3">
+<blockquote className="blockquote mb-0 card-body">
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+    erat a ante.
+  </p>
+  <footer className="blockquote-footer">
+    <small className="text-muted">
+      Someone famous in <cite title="Source Title">Source Title</cite>
+    </small>
+  </footer>
+</blockquote>
+</Card>
+<Card className="text-center">
+<Card.Body>
+  <Card.Title>Card title</Card.Title>
+  <Card.Text>
+    This card has supporting text below as a natural lead-in to additional
+    content.{' '}
+  </Card.Text>
+  <Card.Text>
+    <small className="text-muted">Last updated 3 mins ago</small>
+  </Card.Text>
+</Card.Body>
+</Card>
+<Card>
+<Card.Img src="holder.js/100px160" />
+</Card>
+<Card className="text-right">
+<blockquote className="blockquote mb-0 card-body">
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+    erat a ante.
+  </p>
+  <footer className="blockquote-footer">
+    <small className="text-muted">
+      Someone famous in <cite title="Source Title">Source Title</cite>
+    </small>
+  </footer>
+</blockquote>
+</Card>
+<Card>
+<Card.Body>
+  <Card.Title>Card title</Card.Title>
+  <Card.Text>
+    This is a wider card with supporting text below as a natural lead-in to
+    additional content. This card has even longer content than the first to
+    show that equal height action.
+  </Card.Text>
+  <Card.Text>
+    <small className="text-muted">Last updated 3 mins ago</small>
+  </Card.Text>
+</Card.Body>
+</Card>
+*********************/
