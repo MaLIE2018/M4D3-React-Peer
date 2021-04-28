b53e8e5bec87577f094889431fbcadbe121cb505
MaLIE2018 <liebsch@pm.me>
2021-04-28 15:30:57 +0200

    Ex3

--- peer/src/App.js
+++ peer/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import WarnSign from './components/WarnSign'
+import SingleBook from './components/SingleBook'
+
+const OneBook = {
+  "asin": "0345546792",
+  "title": "The Silent Corner: A Novel of Suspense (Jane Hawk)",
+  "img": "https://images-na.ssl-images-amazon.com/images/I/91dDIYze1wL.jpg",
+  "price": 7.92,
+  "category": "horror"
+};
 
 
 function App() {
   return (
-    <WarnSign message="This is an error!"/>
+    <WarnSign message="This is an error!"/>,
+    <SingleBook book={OneBook}/>
   )
 }
 
---
+++ peer/src/components/SingleBook.jsx
@@ -0,0 +1,14 @@
+import { Card } from "react-bootstrap";
+
+const SingleBook = (props) => {
+  return (
+    <Card>
+      <Card.Img variant='top' src={props.book.img} />
+      <Card.Body>
+        <Card.Title>{props.book.title}</Card.Title>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default SingleBook;
