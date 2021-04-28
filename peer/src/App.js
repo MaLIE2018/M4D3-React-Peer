312a9dc135e8ef06ba0f8b1f9ef2933550567467
Eelismey-jf <80247840+Eelismey-jf@users.noreply.github.com>
2021-04-28 22:57:24 +0200

    Update App.js

--- peer/src/App.js
+++ peer/src/App.js
@@ -1,20 +1,22 @@
-import WarnSign from './components/WarnSign'
-import SingleBook from './components/SingleBook'
-
-const OneBook = {
-  "asin": "0345546792",
-  "title": "The Silent Corner: A Novel of Suspense (Jane Hawk)",
-  "img": "https://images-na.ssl-images-amazon.com/images/I/91dDIYze1wL.jpg",
-  "price": 7.92,
-  "category": "horror"
-};
-
+//import logo from './logo.svg';
+import WarnSign from "./components/WarnSign";
+import Badge from "./components/MyBadge";
+import SingleBook from "./components/SingleBook";
+import ShowBooks from "./components/BookList";
 
 function App() {
   return (
-    <WarnSign message="This is an error!"/>,
-    <SingleBook book={OneBook}/>
-  )
+    <>
+      <WarnSign message="This is an error!" />
+      <Badge color="primary" text="Whatever" />
+      <br />
+      <SingleBook
+        title="The Sign Upon His Head"
+        img="https://images-na.ssl-images-amazon.com/images/I/51sJ-l9UV3L.jpg"
+      />
+      <ShowBooks getMonth="April" />
+    </>
+  );
 }
 
 export default App;
